1c88112de081a637a81eccb24e69aa73fc9a4ef7
lacontreras <lacontreras@intekglobal.com>
2023-01-07 01:35:09 +0000

    Update 6 files

---
+++ src/containers/NewItemFormContainer.js
@@ -0,0 +1,8 @@
+import {connect} from 'react-redux'
+import {NewItemForm} from '../components/NewItemForm'
+
+const mapDispatchToProps = (dispatch)=>{
+    
+}
+
+export const NewItemFormContainer = connect()(NewItemForm)
