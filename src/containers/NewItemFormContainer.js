b3577af804e79ed830b17c01fe9daf33aeb58b77
lacontreras <lacontreras@intekglobal.com>
2023-01-07 01:43:40 +0000

    Update NewItemFormContainer.js with dispatch

--- src/containers/NewItemFormContainer.js
+++ src/containers/NewItemFormContainer.js
@@ -1,8 +1,18 @@
-import {connect} from 'react-redux'
-import {NewItemForm} from '../components/NewItemForm'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { NewItemForm } from '../components/NewItemForm';
+import { addNewItem } from '../store/items/actions';
 
-const mapDispatchToProps = (dispatch)=>{
-    
-}
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      onSubmit: (name, price) => addNewItem(name, price)
+    },
+    dispatch
+  );
+};
 
-export const NewItemFormContainer = connect()(NewItemForm)
+export const NewItemFormContainer = connect(
+  null,
+  mapDispatchToProps
+)(NewItemForm);
